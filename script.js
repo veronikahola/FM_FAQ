4d166f0154fd5913933d343e8300a9a68e71a061
Veronika Holá <veronika.hola@icloud.com>
2023-12-27 10:35:54 +0100

    script icon change

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const containers = document.querySelectorAll('.container');
-const hideShow = (answer) => {
-    answer.classList.toggle('hidden');
-  };
+
 
 for (let i = 0; i < containers.length; i++) {
   const container = containers[i];
   const answer = container.querySelector('.a');
+  const icon = container.querySelector('.img');
+
+  const hideShow = (answer) => {
+
+    if (icon.src.includes('icon-plus')) {
+        icon.src = '/assets/images/icon-minus.svg';
+      } else {
+        icon.src = '/assets/images/icon-plus.svg';
+      }
+
+    answer.classList.toggle('hidden');
+
+  };
 
   container.addEventListener('click', () => hideShow(answer));
 }
